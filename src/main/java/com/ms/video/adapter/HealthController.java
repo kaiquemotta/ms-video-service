d6ed29cd3964842f82bc642d9efe0f359434b000
Kaique Motta <kaique.motta@hotmail.com>
2025-04-19 14:18:11 -0300

    feat: health

--- src/main/java/com/ms/video/adapter/HealthController.java
+++ src/main/java/com/ms/video/adapter/HealthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+
     @GetMapping("/")
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
